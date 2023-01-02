9560ef80164d051b0d5e63fca38d26f3c59ea580
Ning <a24701770@gmail.com>
2023-01-02 16:26:34 +0800

    add delete team & edit turbo frame in index page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
+  before_action :find_team, only: [:edit, :update, :destroy]
 
   def index
     @teams = current_user.teams
@@ -12,15 +13,38 @@ class TeamsController < ApplicationController
     @team = Team.new(team_params)
 
     if @team.save
+      current_user.teams << @team
       redirect_to teams_path
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to teams_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    current_user.teams.destroy(@team)
+    @team.delete
+
+    redirect_to teams_path
+  end
+
   private
 
   def team_params
     params.require(:team).permit(:name, :description)
+  end
+
+  def find_team
+    @team = Team.find(params[:id])
   end
 end
