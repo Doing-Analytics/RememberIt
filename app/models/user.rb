c5918b61186630f35e01785c7d92ab5fa366644b
Ning <a24701770@gmail.com>
2023-01-07 23:41:45 +0800

    rspec: pass user login test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@ class User < ApplicationRecord
 
   # validation
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, presence: true, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, uniqueness: true, format: { with:  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
-
-  validates :email, uniqueness: true
 
   # association
   has_many :team_members
