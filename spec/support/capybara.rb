2881ac9778ac2dd1273492e62511ec4c37ffc4cb
Ted <wodf410@gmail.com>
2023-01-11 21:06:36 +0800

    update config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,10 +4,8 @@ Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.server_port = 3001
 Capybara.app_host = 'http://127.0.0.1:3001'
 
-# https://stackoverflow.com/questions/9784118/how-to-emulate-mouse-hover-with-capybara
-
-Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Firefox::Profile.new
-  profile.native_events = true
-  Capybara::Selenium::Driver.new(app, browser: :firefox, profile:)
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
+
+Capybara.javascript_driver = :selenium_chrome
