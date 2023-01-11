ad6e24a88589804395eca4bfcddb088f8141bf9e
Ted <wodf410@gmail.com>
2023-01-11 21:20:57 +0800

    init request rspec

---
+++ spec/requests/project_boxes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'ProjectBoxes', type: :request do
+  describe 'test routes' do
+  end
+  describe ''
+  describe 'GET /project_boxes' do
+    it 'works! (now write some real specs)' do
+      get project_boxes_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
