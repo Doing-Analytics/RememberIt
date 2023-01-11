ea0a62d888b904eb5ae0fb1ae31a3332a2352b7a
Ted <wodf410@gmail.com>
2023-01-11 20:14:32 +0800

    capybara add config to support hover event

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,3 +3,9 @@
 Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.server_port = 3001
 Capybara.app_host = 'http://127.0.0.1:3001'
+
+Capybara.register_driver :selenium do |app|
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  profile.native_events = true
+  Capybara::Selenium::Driver.new(app, browser: :firefox, profile:)
+end
